21017309943bffaa46842f36853a8855e39b000f
ugongone <yu1931sn@gmail.com>
2025-01-07 08:20:51 +0900

    refactor: improve code clarity and consistency in HomeScreen and RandomPhrase components
    - Standardized import statements and formatting for better readability.
    - Enhanced error handling for API URL checks and fetch operations.
    - Updated animation styles and logic for smoother transitions in card swipe functionality.
    - Improved the handling of phrase rendering in the RandomPhrase component by utilizing useLayoutEffect for state updates.
    - Streamlined the cleanup process for audio playback to prevent memory leaks.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
-import { StyleSheet, useWindowDimensions } from 'react-native';
-import { useState, useEffect } from 'react';
-import { RandomPhrase } from '@/components/RandomPhrase';
-import { ThemedView } from '@/components/ThemedView';
-import { GestureDetector, Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { StyleSheet, useWindowDimensions } from "react-native";
+import { useState, useEffect } from "react";
+import { RandomPhrase } from "@/components/RandomPhrase";
+import { ThemedView } from "@/components/ThemedView";
+import {
+  GestureDetector,
+  Gesture,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface Phrase {
   id: string;
@@ -30,13 +39,13 @@ export default function HomeScreen() {
   useEffect(() => {
     const apiUrl = process.env.EXPO_PUBLIC_API_URL;
     if (!apiUrl) {
-      throw new Error('API_URL is not set');
+      throw new Error("API_URL is not set");
     }
 
     fetch(`${apiUrl}/api/notion/getData`)
-      .then(response => response.json())
-      .then(jsonData => setPhrases(shuffleArray(jsonData.data)))
-      .catch(error => console.error('Error fetching phrases:', error));
+      .then((response) => response.json())
+      .then((jsonData) => setPhrases(shuffleArray(jsonData.data)))
+      .catch((error) => console.error("Error fetching phrases:", error));
   }, []);
 
   // 画面の横幅を取得
@@ -44,7 +53,7 @@ export default function HomeScreen() {
   // Webだと初期描画時にscreenWidthが0になることがあるので、
   // その場合はnullを返して描画をスキップする
   if (screenWidth === 0) {
-    return null
+    return null;
   }
   // カードの横幅を計算
   const cardWidth = screenWidth * 0.8;
@@ -52,9 +61,9 @@ export default function HomeScreen() {
   const peekWidth = 10;
 
   // 各カードの初期位置
-  const initialCardPositionLeft = -screenWidth/2 -cardWidth/2 + peekWidth;
+  const initialCardPositionLeft = -screenWidth / 2 - cardWidth / 2 + peekWidth;
   const initialCardPositionMiddle = 0;
-  const initialCardPositionRight = screenWidth/2 + cardWidth/2 - peekWidth;
+  const initialCardPositionRight = screenWidth / 2 + cardWidth / 2 - peekWidth;
 
   const translateXLeft = useSharedValue(initialCardPositionLeft);
   const translateXMiddle = useSharedValue(initialCardPositionMiddle);
@@ -70,67 +79,85 @@ export default function HomeScreen() {
     .onEnd((event) => {
       // スワイプ距離が左に50pxより大きい場合は、カードを左に移動
       if (event.translationX < -50) {
-        translateXLeft.value = withTiming(initialCardPositionLeft * 2, { duration: 300 });
-        translateXMiddle.value = withTiming(initialCardPositionLeft, { duration: 300 });
-        translateXRight.value = withTiming(initialCardPositionMiddle, { duration: 300 }, () => {
-          // 次のカードを表示するために、カードのインデックスを更新
-          // 状態更新はJSスレッドで行う必要があるため、runOnJSを使用
-          runOnJS(setCurrentPhraseIndex)(prev => prev + 1);
-          translateXLeft.value = initialCardPositionLeft;
-          translateXMiddle.value = initialCardPositionMiddle;
-          translateXRight.value = initialCardPositionRight;
+        translateXLeft.value = withTiming(initialCardPositionLeft * 2, {
+          duration: 300,
         });
+        translateXMiddle.value = withTiming(initialCardPositionLeft, {
+          duration: 300,
+        });
+        translateXRight.value = withTiming(
+          initialCardPositionMiddle,
+          { duration: 300 },
+          () => {
+            // 次のカードを表示するために、カードのインデックスを更新
+            // 状態更新はJSスレッドで行う必要があるため、runOnJSを使用
+            runOnJS(setCurrentPhraseIndex)((prev) => prev + 1);
+            translateXLeft.value = initialCardPositionLeft;
+            translateXMiddle.value = initialCardPositionMiddle;
+            translateXRight.value = initialCardPositionRight;
+          }
+        );
       } else {
         // スワイプ距離が左に50pxより小さい場合は、カードを初期位置に戻す
-        translateXLeft.value = withTiming(initialCardPositionLeft, { duration: 300 });
-        translateXMiddle.value = withTiming(initialCardPositionMiddle, { duration: 300 });
-        translateXRight.value = withTiming(initialCardPositionRight, { duration: 300 });
+        translateXLeft.value = withTiming(initialCardPositionLeft, {
+          duration: 300,
+        });
+        translateXMiddle.value = withTiming(initialCardPositionMiddle, {
+          duration: 300,
+        });
+        translateXRight.value = withTiming(initialCardPositionRight, {
+          duration: 300,
+        });
       }
     });
   const animatedStyleLeft = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: translateXLeft.value }]
+      transform: [{ translateX: translateXLeft.value }],
     };
   });
 
   const animatedStyleMiddle = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: translateXMiddle.value }]
+      transform: [{ translateX: translateXMiddle.value }],
     };
   });
 
   const animatedStyleRight = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: translateXRight.value }]
+      transform: [{ translateX: translateXRight.value }],
     };
   });
 
   return (
     <GestureHandlerRootView>
       <GestureDetector gesture={swipeGesture}>
-        <ThemedView style={styles.container}
+        <ThemedView
+          style={styles.container}
           lightColor="#F5F5F5"
-          darkColor="#F5F5F5">
-
-          <Animated.View style={[
-            styles.cardContainer,
-            animatedStyleLeft,
-          ]}>
-            <RandomPhrase phraseIndex={currentPhraseIndex} phrases={phrases} />
+          darkColor="#F5F5F5"
+        >
+          <Animated.View style={[styles.cardContainer, animatedStyleLeft]}>
+            <RandomPhrase
+              key={currentPhraseIndex}
+              phraseIndex={currentPhraseIndex}
+              phrases={phrases}
+            />
           </Animated.View>
 
-          <Animated.View style={[
-            styles.cardContainer,
-            animatedStyleMiddle,
-          ]}>
-            <RandomPhrase phraseIndex={currentPhraseIndex + 1} phrases={phrases} />
+          <Animated.View style={[styles.cardContainer, animatedStyleMiddle]}>
+            <RandomPhrase
+              key={currentPhraseIndex + 1}
+              phraseIndex={currentPhraseIndex + 1}
+              phrases={phrases}
+            />
           </Animated.View>
 
-          <Animated.View style={[
-            styles.cardContainer,
-            animatedStyleRight,
-          ]}>
-            <RandomPhrase phraseIndex={currentPhraseIndex + 2} phrases={phrases} />
+          <Animated.View style={[styles.cardContainer, animatedStyleRight]}>
+            <RandomPhrase
+              key={currentPhraseIndex + 2}
+              phraseIndex={currentPhraseIndex + 2}
+              phrases={phrases}
+            />
           </Animated.View>
         </ThemedView>
       </GestureDetector>
@@ -143,16 +170,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
-    position: 'relative',
+    position: "relative",
     minHeight: 400,
   },
   cardContainer: {
-    width: '80%',
+    width: "80%",
     maxWidth: 400,
-    alignItems: 'center',
-    position: 'absolute',
-  }
+    alignItems: "center",
+    position: "absolute",
+  },
 });
--- components/RandomPhrase.tsx
+++ components/RandomPhrase.tsx
@@ -1,194 +1,199 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Pressable, Image } from 'react-native';
-import { ThemedText } from './ThemedText';
-import { ThemedView } from './ThemedView';
-import { Audio } from 'expo-av';
+import { useState, useEffect, useLayoutEffect } from "react";
+import { StyleSheet, Pressable, Image } from "react-native";
+import { ThemedText } from "./ThemedText";
+import { ThemedView } from "./ThemedView";
+import { Audio } from "expo-av";
 
 interface Phrase {
-	id: string;
-	english: string;
-	japanese: string;
-	date: string;
+  id: string;
+  english: string;
+  japanese: string;
+  date: string;
 }
 
 interface RandomPhraseProps {
-	phraseIndex: number;
-	phrases: Phrase[];
+  phraseIndex: number;
+  phrases: Phrase[];
 }
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 export function RandomPhrase({ phraseIndex, phrases }: RandomPhraseProps) {
-	const [showEnglish, setShowEnglish] = useState(false);
-	const [sound, setSound] = useState<Audio.Sound | null>(null);
-	
-	const phrase = phrases.length > 0 
-	? phrases[phraseIndex % phrases.length] 
-	: { id: '', english: '', japanese: '', date: '' };
+  const [showEnglish, setShowEnglish] = useState(false);
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
 
-	useEffect(() => {
-		setShowEnglish(false);
-	}, [phraseIndex]);
+  const phrase =
+    phrases.length > 0
+      ? phrases[phraseIndex % phrases.length]
+      : { id: "", english: "", japanese: "", date: "" };
 
-	async function playSound() {
-		try {
-			if (sound) {
-				await sound.unloadAsync();
-			}
+  useLayoutEffect(() => {
+    setShowEnglish(false);
+  }, [phraseIndex]);
 
-			const response = await fetch(`${apiUrl}/api/openai/getVoiceData`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					text: phrase.english,
-				}),
-			});
-			const data = await response.json();
-			const { sound: newSound } = await Audio.Sound.createAsync(
-				{ uri: data.audio },
-				{ shouldPlay: true }
-			);
-			setSound(newSound);
-			await newSound.playAsync();
-		} catch (error) {
-			console.error('Error playing sound:', error);
-		}
-	}
+  async function playSound() {
+    try {
+      if (sound) {
+        await sound.unloadAsync();
+      }
 
-	// コンポーネントがアンマウントされる際に、サウンドをクリーンアップしてメモリを解放
-	useEffect(() => {
-		return sound ? () => { sound.unloadAsync(); } : undefined;
-	}, [sound]);
+      const response = await fetch(`${apiUrl}/api/openai/getVoiceData`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: phrase.english,
+        }),
+      });
+      const data = await response.json();
+      const { sound: newSound } = await Audio.Sound.createAsync(
+        { uri: data.audio },
+        { shouldPlay: true }
+      );
+      setSound(newSound);
+      await newSound.playAsync();
+    } catch (error) {
+      console.error("Error playing sound:", error);
+    }
+  }
 
-	return (
-		<ThemedView style={styles.container}>
-			<ThemedView style={styles.card}>
-				<ThemedView style={styles.contentContainer}>
-					<ThemedView style={styles.phraseContainer}>
-						<ThemedText type="title" style={styles.japanese}>
-							{phrase.japanese}
-						</ThemedText>
-						<ThemedView style={styles.englishContainer}>
-							{showEnglish ? (
-								<ThemedView style={styles.englishWrapper}>
-									<ThemedText type="title" style={styles.english}>
-										{phrase.english}
-									</ThemedText>
-									<Pressable onPress={playSound}>
-										<Image 
-											source={require('../assets/images/speaker-filled-audio-tool.png')}
-											style={styles.logo}
-										/>
-									</Pressable>
-								</ThemedView>	
-							) : (
-								<ThemedView style={styles.placeholder} />
-							)}
-						</ThemedView>
-					</ThemedView>
+  // コンポーネントがアンマウントされる際に、サウンドをクリーンアップしてメモリを解放
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
-					<Pressable
-						onPress={(e) => {
-							e.stopPropagation();
-							setShowEnglish(!showEnglish);
-						}}
-						style={({ pressed }) => [
-							styles.button,
-							pressed && styles.buttonPressed
-						]}
-					>
-						<ThemedText>
-							{showEnglish ? "英語を非表示" : "英語を表示"}
-						</ThemedText>
-					</Pressable>
-				</ThemedView>
-			</ThemedView>
-		</ThemedView>
-	);
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.card}>
+        <ThemedView style={styles.contentContainer}>
+          <ThemedView style={styles.phraseContainer}>
+            <ThemedText type="title" style={styles.japanese}>
+              {phrase.japanese}
+            </ThemedText>
+            <ThemedView style={styles.englishContainer}>
+              {showEnglish ? (
+                <ThemedView style={styles.englishWrapper}>
+                  <ThemedText type="title" style={styles.english}>
+                    {phrase.english}
+                  </ThemedText>
+                  <Pressable onPress={playSound}>
+                    <Image
+                      source={require("../assets/images/speaker-filled-audio-tool.png")}
+                      style={styles.logo}
+                    />
+                  </Pressable>
+                </ThemedView>
+              ) : (
+                <ThemedView style={styles.placeholder} />
+              )}
+            </ThemedView>
+          </ThemedView>
+
+          <Pressable
+            onPress={(e) => {
+              e.stopPropagation();
+              setShowEnglish(!showEnglish);
+            }}
+            style={({ pressed }) => [
+              styles.button,
+              pressed && styles.buttonPressed,
+            ]}
+          >
+            <ThemedText>
+              {showEnglish ? "英語を非表示" : "英語を表示"}
+            </ThemedText>
+          </Pressable>
+        </ThemedView>
+      </ThemedView>
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		alignItems: 'center',
-		width: '100%',
-	},
-	card: {
-		backgroundColor: '#FFFFFF',
-		borderRadius: 16,
-		width: '100%',
-		maxWidth: 550,
-		height: 360,
-		shadowColor: 'rgba(0, 0, 0, 0.1)',
-		shadowOffset: {
-			width: 0,
-			height: 2,
-		},
-		shadowOpacity: 0.2,
-		shadowRadius: 8,
-		elevation: 4,
-		borderWidth: 0,
-	},
-	contentContainer: {
-		padding: 24,
-		flex: 1,
-		justifyContent: 'space-between',
-	},
-	phraseContainer: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		paddingVertical: 16,
-		position: 'relative',
-	},
-	englishContainer: {
-		minHeight: 48,
-		justifyContent: 'center',
-		alignItems: 'center',
-		marginTop: 16,
-		position: 'absolute',
-		bottom: 40,
-		width: '100%',
-	},
-	englishWrapper: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: '100%',
-	},
-	logo: {
-		width: 24,
-		height: 24,
-		resizeMode: 'contain',
-	},
-	placeholder: {
-		height: 48,
-	},
-	japanese: {
-		fontSize: 20,
-		textAlign: 'center',
-		paddingHorizontal: 16,
-		position: 'absolute',
-		top: 40,
-		width: '100%',
-		lineHeight: 28,
-	},
-	english: {
-		fontSize: 20,
-		textAlign: 'center',
-		paddingHorizontal: 16,
-		flex: 1,
-	},
-	button: {
-		padding: 12,
-		borderRadius: 8,
-		backgroundColor: '#A1CEDC',
-		alignItems: 'center',
-		alignSelf: 'center',
-		minWidth: 120,
-	},
-	buttonPressed: {
-		opacity: 0.7,
-	}
-}); 
+  container: {
+    alignItems: "center",
+    width: "100%",
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 16,
+    width: "100%",
+    maxWidth: 550,
+    height: 360,
+    shadowColor: "rgba(0, 0, 0, 0.1)",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 4,
+    borderWidth: 0,
+  },
+  contentContainer: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  phraseContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 16,
+    position: "relative",
+  },
+  englishContainer: {
+    minHeight: 48,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 16,
+    position: "absolute",
+    bottom: 40,
+    width: "100%",
+  },
+  englishWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  logo: {
+    width: 24,
+    height: 24,
+    resizeMode: "contain",
+  },
+  placeholder: {
+    height: 48,
+  },
+  japanese: {
+    fontSize: 20,
+    textAlign: "center",
+    paddingHorizontal: 16,
+    position: "absolute",
+    top: 40,
+    width: "100%",
+    lineHeight: 28,
+  },
+  english: {
+    fontSize: 20,
+    textAlign: "center",
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  button: {
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: "#A1CEDC",
+    alignItems: "center",
+    alignSelf: "center",
+    minWidth: 120,
+  },
+  buttonPressed: {
+    opacity: 0.7,
+  },
+});
